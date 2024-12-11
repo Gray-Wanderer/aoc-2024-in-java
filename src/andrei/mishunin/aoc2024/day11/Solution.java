19a44f02d7eae00b61af4ce948bb834441a3c09c
Andrei Mishunin <andrei@altacogni.com>
2024-12-11 08:26:10 +0100

    Day 11

--- src/andrei/mishunin/aoc2024/day11/Solution.java
+++ src/andrei/mishunin/aoc2024/day11/Solution.java
@@ -11,37 +11,6 @@ import java.util.Map;
 public class Solution {
     static Map<String, Map<Integer, BigInteger>> TRANSFORM_COUNT = new HashMap<>();
 
-    public static long solve(String file, int iterations) {
-        List<String> input = InputReader.readAllLines(file);
-        String[] numbers = input.get(0).split(" ");
-
-        NumberNode head = new NumberNode(numbers[0]);
-        NumberNode current = head;
-        for (int i = 1; i < numbers.length; i++) {
-            NumberNode node = new NumberNode(numbers[i]);
-            node.prev = current;
-            current.next = node;
-            current = node;
-        }
-
-        for (int i = 0; i < iterations; i++) {
-            NumberNode node = head;
-            while (node != null) {
-                node = node.transform().next;
-            }
-//            System.out.println();
-        }
-
-        long count = 0;
-        NumberNode node = head;
-        while (node != null) {
-            node = node.next;
-            count++;
-        }
-
-        return count;
-    }
-
     public static String solve2(String file, int iterations) {
         List<String> input = InputReader.readAllLines(file);
         String[] numberS = input.get(0).split(" ");
@@ -74,7 +43,7 @@ public class Solution {
             }
 
             if (BigInteger.ZERO.compareTo(pair.num) == 0) {
-                memo = getFromMemo(pair.num, pair.step - 1);
+                memo = getFromMemo(BigInteger.ONE, pair.step - 1);
                 if (memo == null) {
                     stack.addLast(pair);
                     stack.addLast(new Pair(BigInteger.ONE, pair.step - 1));
@@ -82,10 +51,8 @@ public class Solution {
                     putToMemo(pair.num, pair.step, memo);
                 }
             } else if (numberS.length() % 2 == 0) {
-                String s1 = numberS.substring(0, numberS.length() / 2);
-                BigInteger n1 = new BigInteger(s1);
-                String s2 = numberS.substring(numberS.length() / 2);
-                BigInteger n2 = new BigInteger(s2);
+                BigInteger n1 = new BigInteger(numberS.substring(0, numberS.length() / 2));
+                BigInteger n2 = new BigInteger(numberS.substring(numberS.length() / 2));
                 BigInteger memo1 = getFromMemo(n1, pair.step - 1);
                 BigInteger memo2 = getFromMemo(n2, pair.step - 1);
                 if (memo1 != null && memo2 != null) {
@@ -106,7 +73,7 @@ public class Solution {
                     stack.addLast(pair);
                     stack.addLast(new Pair(nextNumber, pair.step - 1));
                 } else {
-                    putToMemo(nextNumber, pair.step, memo);
+                    putToMemo(pair.num, pair.step, memo);
                 }
             }
         }
@@ -133,59 +100,17 @@ public class Solution {
         return null;
     }
 
-    private record Pair(BigInteger num, int step) {
-    }
-
     public static void main(String[] args) {
         System.out.println("== TEST 1 ==");
         System.out.println(solve2("day11/test.txt", 25));
-//        System.out.println("== SOLUTION 1 ==");
-//        System.out.println(solve("day11/input.txt", 25));
-//        System.out.println("== TEST 2 ==");
-//        System.out.println(solve("day11/test.txt"));
-//        System.out.println("== SOLUTION 2 ==");
-//        System.out.println(solve("day11/input.txt", 75));
+        System.out.println("== SOLUTION 1 ==");
+        System.out.println(solve2("day11/input.txt", 25));
+        System.out.println("== TEST 2 ==");
+        System.out.println(solve2("day11/test.txt", 75));
+        System.out.println("== SOLUTION 2 ==");
+        System.out.println(solve2("day11/input.txt", 75));
     }
 
-    private static class NumberNode {
-        String numberS;
-        BigInteger number;
-        NumberNode prev = null;
-        NumberNode next = null;
-
-        public NumberNode(String numberS) {
-            this.numberS = numberS;
-            this.number = new BigInteger(numberS);
-        }
-
-        public NumberNode(BigInteger number) {
-            this.numberS = number.toString();
-            this.number = number;
-        }
-
-        public NumberNode transform() {
-            if (BigInteger.ZERO.compareTo(number) == 0) {
-                numberS = "1";
-                number = BigInteger.ONE;
-//                System.out.print(number + " ");
-                return this;
-            }
-            if (numberS.length() % 2 == 0) {
-                BigInteger s1 = new BigInteger(numberS.substring(0, numberS.length() / 2));
-                BigInteger s2 = new BigInteger(numberS.substring(numberS.length() / 2));
-                numberS = s1.toString();
-                number = s1;
-                NumberNode newNode = new NumberNode(s2);
-                newNode.next = this.next;
-                newNode.prev = this;
-                this.next = newNode;
-//                System.out.print(s1 + " " + s2 + " ");
-                return newNode;
-            }
-            number = number.multiply(BigInteger.valueOf(2024));
-            numberS = number.toString();
-//            System.out.print(number + " ");
-            return this;
-        }
+    private record Pair(BigInteger num, int step) {
     }
 }
